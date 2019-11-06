34e5121b1d20649c4d184a34ae9d81005f2e7615
Justin Richling <36716747+MethaneRain@users.noreply.github.com>
2019-11-06 11:42:40 -0700

    Update Kinematics1d.java

--- Kinematics1d.java
+++ Kinematics1d.java
@@ -2,26 +2,28 @@
 
 public class Kinematics1d {
 	
-	double Disp(double x0, double v0, double t) {
+	double Disp(double x0, double v0, double t, int i) {
 	//double x = x0 + (v0*Math.cos(angle)*t);	
-	double x = x0 + (v0*t);	
+	double a = 0.2;
+	double x = x0 + (v0*t)-a*t*t;	
 
-	System.out.println(x+" meters");
+	System.out.println("\n"+"time step: "+i+"\nx-displacement: "+x+" meters");
 	return x;
 	}
 	
 	public static void main(String[] args) {
-		double v0 = 1.54;
+		double v0 = 10.54;
 		double t = 1;
-		double x0 = 10;
+		double x0 = 0;
 	
 		int k = 5;
 		for (int i = 0; i < k; i++) {
 			Kinematics1d disp = new Kinematics1d();
-			double X = disp.Disp(x0,v0,t+i);
+			double X = disp.Disp(x0,v0,t+i,i+1);
 			x0 = X;
+	
 			}
-		
+	System.out.println("\nDone.");	
 	}
 	
 
